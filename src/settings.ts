4a97dab1eb9f41adb418b58e105579b003b80f6f
rtuszik <robin@tuszik.de>
2023-12-01 15:46:41 +0100

    Update Config Schema and Settings for Accurate Platform Registration

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,5 @@
-/**
- * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
- */
-export const PLATFORM_NAME = 'V-ZUG';
 
-/**
- * This must match the name of your plugin as defined the package.json
- */
+export const PLATFORM_NAME = 'homebridge-vzug';
+
+
 export const PLUGIN_NAME = 'homebridge-vzug';
